1daeabee8cec3dacbe8a47af2ee88d70363611ae
nemsime <tumanyanmarianna79@gmail.com>
2026-03-13 23:55:57 +0400

    vrode norm vrode ne

--- Cub3D/src/game.c
+++ Cub3D/src/game.c
@@ -1,50 +1,10 @@
 #include "../include/cub3d.h"
 
-void	free_map(t_map *map)
-{
-	t_map_node	*tmp;
-	t_map_node	*next;
-
-	if (!map)
-		return;
-	tmp = map->head;
-	while (tmp)
-	{
-		next = tmp->next;
-		free(tmp->line);
-		free(tmp);
-		tmp = next;
-	}
-	map->head = NULL;
-	map->width = 0;
-	map->height = 0;
-}
-
-void	free_assets(t_game *game)
-{
-	if (game->assets.no)
-		free(game->assets.no);
-	if (game->assets.so)
-		free(game->assets.so);
-	if (game->assets.we)
-		free(game->assets.we);
-	if (game->assets.ea)
-		free(game->assets.ea);
-}
-
-void	free_game_content(t_game *game)
-{
-	if (!game)
-		return;
-
-	free_assets(game);
-	free_map(&game->map);
-
-}
-
 int	main(int argc, char **argv)
 {
 	t_game game;
 	validation_stage(argc, argv, &game);
+	printf("%d\n",game.map.width);
+	printf("%d\n",game.map.height);
 	free_game_content(&game);
 }
